2957ecd9d0d939b9d6791091eff3e50cb46db13f
paulina994 <paulina.koleva994@gmail.com>
2023-01-25 19:12:31 +0200

    add tasks
    exercise

---
+++ LAB/SetsAndMaps/LargestThreeNumbers.java
@@ -0,0 +1,27 @@
+package SetsAndMaps;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LargestThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] input  = scanner.nextLine().split(" ");
+        List<Integer> numbers = new ArrayList<>();
+
+        for(String elem : input){
+            numbers.add(Integer.parseInt(elem));
+        }
+        List<Integer> sorted = numbers
+                .stream()
+                .sorted((l, r) -> r.compareTo(l))
+                .collect(Collectors.toList());
+
+        int limit = Math.min(sorted.size(), 3);
+        for (int i = 0; i < limit; i++) {
+            System.out.print(sorted.get(i) + " ");
+
+        }
+    }
+}
