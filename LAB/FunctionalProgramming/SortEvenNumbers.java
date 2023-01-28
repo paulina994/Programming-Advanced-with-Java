3173930c5ea75d5738c4c70b7d381c4067097367
paulina994 <paulina.koleva994@gmail.com>
2023-01-28 20:02:40 +0200

    add tasks
    exercise

---
+++ LAB/FunctionalProgramming/SortEvenNumbers.java
@@ -0,0 +1,38 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SortEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(elem -> Integer.parseInt(elem))
+                .collect(Collectors.toList());
+
+        numbers.removeIf(num -> num % 2 != 0);
+
+        printWithSeparator(numbers);
+
+        System.out.println();
+        List<Integer> sortedNumbers = numbers
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+printWithSeparator(sortedNumbers);
+    }
+
+    private static void printWithSeparator(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+
+            if (i < numbers.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
