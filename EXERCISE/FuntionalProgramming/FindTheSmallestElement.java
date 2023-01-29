cb7d6d1e94b663eed61dc0c25ba63dbe2694988d
paulina994 <paulina.koleva994@gmail.com>
2023-01-29 15:41:31 +0200

    add tasks
    exercise

---
+++ EXERCISE/FuntionalProgramming/FindTheSmallestElement.java
@@ -0,0 +1,28 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+
+public class FindTheSmallestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Consumer<List<Integer>> printLastIndexOfMinElement = list -> {
+            int minElement = Collections.min(list);
+            System.out.println(list.lastIndexOf(minElement));
+        };
+        printLastIndexOfMinElement.accept(numbers);
+    }
+
+
+}
+
